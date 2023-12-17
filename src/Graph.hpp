81761ec9071aae35f31b229f4bd80209e207136e
eeeeagle <d.d.osokin@mail.ru>
2023-12-17 21:26:44 +0400

    Добавьте файлы проекта.

---
+++ src/Graph.hpp
@@ -0,0 +1,163 @@
+﻿#include <list>
+#include <map>
+#include <queue>
+#include <iostream>
+#include <string>
+
+const int VERTICES = 2;
+
+class Graph
+{
+private:
+	typedef std::map<int, int> VertexMap; // key - destination Edge, value - weight
+	typedef std::map<int, VertexMap> EdgeMap; // key - Edge id, value - list of edge vertexes;
+	
+	EdgeMap data;
+public:
+	Graph() 
+		: data()
+	{}
+
+	void insert_edge(const int id)
+	{		
+		if (data.contains(id) || id < 0)
+			return;
+
+		data.insert(std::make_pair(id, VertexMap()));
+	}
+
+	void delete_edge(const int id)
+	{
+		if (!data.contains(id))
+			return;
+
+		for (auto&& it_edge : data)
+			delete_vertex(it_edge.first, id);
+		data.erase(id);
+	}
+
+	void insert_vertex(const int src, const int dest, const int weight)
+	{
+		if (!data.contains(src) || !data.contains(dest) || data[src].contains(dest) || src < 0 || dest < 0 || weight < 0)
+			return;
+
+		data[src].insert(std::make_pair(dest, weight));
+	}
+
+	void delete_vertex(const int src, const int dest)
+	{
+		if (!data.contains(src) || !data.contains(dest))
+			return;
+
+		data[src].erase(dest);
+	}
+
+	void print() const
+	{
+		for (auto&& it_edge : data)
+		{
+			std::cout << "EDGE [" << it_edge.first << ']' << std::endl;
+			for (auto&& it_vertex : it_edge.second)
+				std::cout << "dst: " << it_vertex.first << " | weight: " << it_vertex.second << std::endl;
+			std::cout << std::endl;
+		}
+	}
+
+	int operator()(const int src, const int dest) const
+	{        
+        for (auto&& it_vertex : data.find(src)->second)
+            if (it_vertex.first == dest)
+                return it_vertex.second;
+
+		return 0;
+	}
+
+    void find_shortest_way(const int src) const
+    {
+        if (!data.contains(src))
+            return;
+
+        std::map<int, bool> visited;
+        std::map<int, int> distances;
+        unsigned min_weight, min_index;
+
+        for (auto&& it : data)
+        {
+            visited.insert(std::make_pair(it.first, false));
+            distances.insert(std::make_pair(it.first, INT_MAX));
+        }
+        distances[src] = 0;
+
+        do
+        {
+            min_index = INT_MAX;
+            min_weight = INT_MAX;
+
+            for (auto&& it : data)
+            {
+                if (!visited[it.first] && distances[it.first] < min_weight)
+                {
+                    min_index = it.first;
+                    min_weight = distances[it.first];
+                }
+            }
+
+            if (min_index != INT_MAX)
+            {
+                for (auto&& it : data)
+                {
+                    if ((*this)(min_index, it.first))
+                    {
+                        unsigned temp = min_weight + (*this)(min_index, it.first);
+
+                        if (temp < distances[it.first])
+                            distances[it.first] = temp;
+                    }
+                }
+
+                visited[min_index] = true;
+            }
+        } while (min_index < INT_MAX);
+
+        for (auto&& it_i : data)
+        {
+            if (distances[it_i.first] != INT_MAX)
+            {
+                std::cout << '[' << it_i.first << "] Weight = " << std::left << distances[it_i.first] << "\t";
+
+                unsigned end = it_i.first;
+                unsigned weight = distances[end];
+                std::string way = std::to_string(end) + " >- ";
+
+                while (end != src)
+                {
+                    for (auto&& it_j : data)
+                    {
+                        if ((*this)(it_j.first, end))
+                        {
+                            int temp = weight - (*this)(it_j.first, end);
+
+                            if (temp == distances[it_j.first])
+                            {
+                                end = it_j.first;
+                                weight = temp;
+                                way += std::to_string(it_j.first) + " >- ";
+                            }
+                        }
+                    }
+                }
+
+                std::cout << " | Path: ";
+
+                for (int j = way.length() - 5; j >= 0; --j)
+                    std::cout << way[j];
+
+                std::cout << std::endl;
+            }
+            else
+                std::cout << '[' << it_i.first << "] path unnable" << std::endl;
+        }
+
+    }
+};
+
---
+++ src/PM_9.cpp
@@ -0,0 +1,91 @@
+﻿#include "Graph.hpp"
+#include <string>
+#include <conio.h>
+
+bool is_value(const std::string& str)
+{
+    for (auto it : str)
+        if (!isdigit(it))
+            return false;
+    return !str.empty();
+}
+
+int get_int(const std::string& text)
+{
+    while (true)
+    {
+        std::cout << text;
+        std::string input_string;
+        getline(std::cin, input_string);
+
+        if (is_value(input_string))
+            return std::stoi(input_string);
+        else
+            std::cout << "Incorrect input. Try again." << std::endl;
+    }
+}
+
+int get_key()
+{
+    int key = _getch();
+    if ((key == 0) || (key == 224))
+        key = _getch();
+    return key - 48;
+}
+
+int main_menu()
+{
+    std::cout << "[1] Insert edge" << std::endl;
+    std::cout << "[2] Insert vertex" << std::endl;
+    std::cout << "[3] Print" << std::endl;
+    std::cout << "[4] Find shortest way" << std::endl;
+    std::cout << "[5] Exit" << std::endl;
+
+    while (true)
+    {
+        int key = get_key();
+        if (key == 1 || key == 2 || key == 3 || key == 4 || key == 5)
+            return key;
+    }
+}
+
+int main()
+{
+    Graph graph;
+    std::string buffer;
+    while (true)
+    {
+        int key = main_menu();
+        system("cls");
+        if (key == 5)
+            return 0;
+        else if (key == 1)
+        {
+            key = get_int("Add edge (id): ");
+            graph.insert_edge(key);
+        }
+        else if (key == 2)
+        {
+            struct Vertex 
+            {
+                int src, dest, weight;
+            } vertex(0, 0, 0);
+            vertex.src = get_int("Enter source edge (id): ");
+            vertex.dest = get_int("Enter destination edge (id): ");
+            vertex.weight = get_int("Enter vertex weight: ");
+            graph.insert_vertex(vertex.src, vertex.dest, vertex.weight);
+        }
+        else if (key == 3)
+        {
+            graph.print();
+            system("pause");
+        }
+        else if (key == 4)
+        {
+            key = get_int("Find shortest ways from edge (id): ");
+            graph.find_shortest_way(key);
+            system("pause");
+        }
+        system("cls");
+    }
+}
